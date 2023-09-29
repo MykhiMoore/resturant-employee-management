442b036d9d5537f4c948daef60768b9913087b34
MykhiMoore <moore_mykhi@student.mahoningctc.com>
2023-09-29 11:25:47 -0400

    Add project files.

---
+++ resturant employee management/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resturant_employee_management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Restaurant resturant = new Restaurant();
+
+            resturant.AddEmployee(new Employee(1, "jake", "jones", "Manager", 9000, DateTime.Parse("2022-01-15")));
+            resturant.AddEmployee(new Employee(2, "john", "james", "chef", 30000, DateTime.Parse("2022-11-15")));
+            resturant.AddEmployee(new Employee(3, "justin", "biber", "assisstant manager", 70000, DateTime.Parse("2022-10-19")));
+            resturant.AddEmployee(new Employee(4, "shawn", "taylor", "waiter", 30000, DateTime.Parse("2022-04-12")));
+
+            resturant.DisplayAllEmployees();
+
+            Console.ReadLine();
+
+        }
+
+
+
+    }
+}
---
+++ resturant employee management/Restaurant.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resturant_employee_management
+{
+    internal class Restaurant
+    {
+        // Field
+        private List<Employee> employees = new List<Employee>();
+
+        // Method to add an employee
+        public void AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+        // Method to remove an employee based on their id
+        public void RemoveEmployee(int id)
+        {
+            employees.RemoveAll(e => e.Id == id);
+        }
+
+        // Method to retrieve details of an employee based on their id
+        public Employee GetEmployee(int id)
+        {
+            return employees.Find(e => e.Id == id);
+        }
+
+        // Method to display details of all employees
+        public void DisplayAllEmployees()
+        {
+            Console.WriteLine("List of Employees:");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"ID: {employee.Id}, Name: {employee.FullName()}, Role: {employee.Role}, Salary: {employee.Salary:C}, Date of Hire: {employee.DateOfHire:d}");
+            }
+        }
+    }
+}
